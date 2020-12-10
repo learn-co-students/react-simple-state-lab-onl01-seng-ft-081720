d69d07bca30f917f10bb5cf2a8fa8cee81cf558d
Tabatha Slatton <valbatha@gmail.com>
2020-12-09 19:35:40 -0700

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,7 +5,7 @@ export default class Matrix extends Component {
   
   genRow = (vals) => {
     // console.log(vals)
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    return vals.map(val => <Cell value={val} />) // replace me and render a cell component instead!
   }
   
   genMatrix = () => {
@@ -16,9 +16,8 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="matrix">
-        {this.genMatrix()}
+      {this.genMatrix()}
       </div>
-      
     )
   }
   
